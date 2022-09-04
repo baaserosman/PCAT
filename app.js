555fc64ff7e7d2e5fd0b876ce7b5df1180c76a65
baaserosman <baaser.osman@gmail.com>
2022-09-04 11:02:37 +0200

    tekli foto sayfaları

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+//! 3rd party modules
 const express = require('express');
+const mongoose = require('mongoose');
+//! Modules in the core structure of Node
 const ejs = require('ejs');
 const path = require('path');
+//! my own created files
+const Photo = require('./models/Photo')
 
 const app = express();
+
+//* Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //* TEMPLATE ENGINE 
 app.set("view engine", "ejs");
@@ -13,10 +24,23 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
 //* ROUTES
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     // res.sendFile(path.resolve(__dirname, "temp/index.html"))
-    res.render("index")
+    const photos = await Photo.find({})
+    res.render("index", {
+      photos
+    })
 })
+             //? Photo'ya tıklandığında gönderilen id yi yakalamak için parametre olarak id veriyorum. Herhangi bir isim de veribilirdik.
+             //? req.params.id ile parametre olarak gönderilen id'yi yakalıyoruz. http://expressjs.com/en/5x/api.html#req.params
+app.get('/photos/:id', async (req, res) => {
+  // console.log(req.params.id);
+  // res.render('about');
+  const photo = await Photo.findById(req.params.id)
+  res.render("photo", {
+    photo
+  })
+});
 
 app.get('/about', (req, res) => {
   res.render('about');
@@ -26,8 +50,8 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
   res.redirect("/")
 });
 
